19c23337283d119bc15a913002c0814fab605174
doniahusien <doniahusien@gmail.com>
2025-08-22 13:16:41 +0300

    fix form input

--- src/components/Form/FormInput.jsx
+++ src/components/Form/FormInput.jsx
@@ -13,17 +13,18 @@ const FormInput = ({ type, placeholder, name, validation, label ,register,errors
                     {label}
                 </label>
                 <motion.input
+                    id={name}
                     type={type}
                     {...register( name , validation)}
                     className={`w-full px-4 text-black py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-400 ${errors.name ? "border-red-500" : "border-gray-300"
                         }`}
-                    whileFocus={{ scale: 1.03, boxShadow: "0 0 5px #fbbf24" }}
+                    whileFocus={{ scale: 1.03 }}
                     whileHover={{ scale: 1.02 }}
                     placeholder={placeholder}
                 />
-                {errors.name && (
+                {errors[name]&& (
                     <span className="text-red-500 text-sm mt-1">
-                        {errors.name.message}
+                        {errors[name].message}
                     </span>
                 )}
             </div>
